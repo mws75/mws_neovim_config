5a019ca241cd4f19ee39f85409eafe1715ae8d19
mws75 <mwspencer75@gmail.com>
2025-11-03 09:02:37 -0800

    cleaned up init.lua file

--- init.lua
+++ init.lua
@@ -16,52 +16,48 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Plugin setup
+-- ============================================================================
+-- Plugin Setup
+-- ============================================================================
 require("lazy").setup({
-  -- Tokyo Night colorscheme
+  -- Colorscheme
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
       require("tokyonight").setup({
-        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        style = "night",
         light_style = "day",
         transparent = false,
         terminal_colors = true,
         styles = {
           comments = { italic = true },
           keywords = { italic = true },
-          functions = {},
-          variables = {},
           sidebars = "dark",
           floats = "dark",
         },
         sidebars = { "qf", "help" },
         day_brightness = 0.3,
-        hide_inactive_statusline = false,
-        dim_inactive = false,
-        lualine_bold = false,
-        on_colors = function(colors) end,
-        on_highlights = function(highlights, colors) end,
       })
       vim.cmd.colorscheme("tokyonight")
     end,
   },
+  -- Formatting
   {
     "stevearc/conform.nvim",
     config = function()
       require("conform").setup({
         formatters_by_ft = {
-          lua = {"stylua"},
-          javascript = {"prettier"},
-          typescript = {"prettier"},
-          javascriptreact = {"prettier"},
-          typescriptreact = {"prettier"},
-          json = {"prettier"},
-          css = {"prettier"},
-          html = {"prettier"},
-          markdown = {"prettier"},
+          lua = { "stylua" },
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          json = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+          markdown = { "prettier" },
         },
         format_on_save = {
           timeout_ms = 500,
@@ -70,28 +66,30 @@ require("lazy").setup({
       })
     end,
   },
+  -- Linting
   {
     "mfussenegger/nvim-lint",
-    event = {"BufReadPre", "BufNewFile"},
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       local lint = require("lint")
       lint.linters_by_ft = {
-        typescript = {"eslint"},
-        javascript = {"eslint"},
-        typescriptreact = {"eslint"},
-        javascriptreact = {"eslint"},
+        typescript = { "eslint" },
+        javascript = { "eslint" },
+        typescriptreact = { "eslint" },
+        javascriptreact = { "eslint" },
       }
-     -- Auto lint on save
-     local lint_augroup = vim.api.nvim_create_augroup("lint", {clear = true})
 
-     vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost", "InsertLeave"}, {
+      -- Auto lint on save
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         group = lint_augroup,
         callback = function()
           lint.try_lint()
         end,
-     })
-   end,
+      })
+    end,
   },
+  -- Syntax Highlighting
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -115,19 +113,18 @@ require("lazy").setup({
         indent = {
           enable = true,
         },
-        -- Enable matchup integration
         matchup = {
           enable = true,
         },
       })
     end,
   },
+  -- Matching Tag Highlighting
   {
     "andymass/vim-matchup",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     event = { "BufReadPost", "BufNewFile" },
     init = function()
-      -- These need to be set BEFORE the plugin loads
       vim.g.matchup_matchparen_enabled = 1
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
       vim.g.matchup_matchparen_deferred = 1
@@ -136,29 +133,22 @@ require("lazy").setup({
       vim.g.matchup_surround_enabled = 1
     end,
     config = function()
-      -- Make highlighting more visible
-      vim.api.nvim_set_hl(0, "MatchParen", {
-        bg = "#3d59a1",
-        fg = "#ffffff",
-        bold = true,
-        underline = true
-      })
-      vim.api.nvim_set_hl(0, "MatchWord", {
-        bg = "#3d59a1",
-        fg = "#ffffff",
-        bold = true,
-        underline = true
-      })
-      vim.api.nvim_set_hl(0, "MatchParenOffscreen", {
-        bg = "#5a7bb1",
-        fg = "#ffffff",
-        bold = true
-      })
+      -- Set custom highlight colors for matching tags
+      local highlights = {
+        MatchParen = { bg = "#3d59a1", fg = "#ffffff", bold = true, underline = true },
+        MatchWord = { bg = "#3d59a1", fg = "#ffffff", bold = true, underline = true },
+        MatchParenOffscreen = { bg = "#5a7bb1", fg = "#ffffff", bold = true },
+      }
+      for group, opts in pairs(highlights) do
+        vim.api.nvim_set_hl(0, group, opts)
+      end
     end,
   },
 })
 
--- Basic editor settings
+-- ============================================================================
+-- Editor Settings
+-- ============================================================================
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
@@ -174,18 +164,15 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
-
--- Use System Clipboard by default
 vim.opt.clipboard = "unnamedplus"
 
--- Key Mappings 
+-- ============================================================================
+-- Keymaps
+-- ============================================================================
 -- Map jj to escape (popular alternative for touch bar keyboards)
-vim.keymap.set({'i', 'v', 'c'}, 'jj', '<Esc>', {
-  noremap = true, silent = true}) 
+vim.keymap.set({ "i", "v", "c" }, "jj", "<Esc>", { noremap = true, silent = true })
 
-vim.keymap.set({'n', 'v'}, '<leader>f', function()
-  require("conform").format({async = true, lsp_fallback = true })
-end, {desc = "Format buffer" }) 
-
-
-
+-- Format buffer
+vim.keymap.set({ "n", "v" }, "<leader>f", function()
+  require("conform").format({ async = true, lsp_fallback = true })
+end, { desc = "Format buffer" })
